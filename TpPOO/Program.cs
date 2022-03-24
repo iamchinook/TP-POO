1a115d5ddb3773f5dbd2e53ad874ea4a534184e4
Chino <Chino@DESKTOP-F4R0G5L>
2022-03-24 10:05:21 -0300

    Agregar archivos de proyecto.

---
+++ TpPOO/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TpPOO
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Persona Persona1 = new Persona("Alejandro", 40000, "M", 70, 165);
+            string nombre = Persona1.Nombre;
+            int sueldo = Persona1.Sueldo;
+            string sexo = Persona1.Sexo;
+            int peso = Persona1.Peso;
+            int altura = Persona1.Altura;
+            //string mail = CrearMail();
+            string mail = Persona1.Mail;
+        }
+
+        public class Persona
+        {
+            public string Nombre { get; set; }
+            public string Mail { get; set; }
+            public int Sueldo { get; set; }
+            public string Sexo { get; set; }
+            public int Peso { get; set; }
+            public int Altura { get; set; }
+
+            public Persona (string _nombre, int _sueldo, string _sexo, int _peso, int _altura)
+            {
+                Nombre = _nombre;
+                Sueldo = _sueldo;
+                Sexo = _sexo;
+                Peso = _peso;
+                Altura = _altura;
+                //Mail = _nombre + "@ejercito.mil.ar";
+                private string CrearMail()
+                {
+                    Mail = _nombre + "@ejercito.mil.ar";
+                    return Mail;
+                }
+                Mail = CrearMail();
+            }
+
+
+        }
+    }
+}
---
+++ TpPOO/Program2.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TpPOO
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Ingrese su nombre: ");
+            string _nombre = Console.ReadLine();
+
+            Console.WriteLine("Ingrese su sueldo: ");
+            string _sueldo = Console.ReadLine();
+
+            Console.WriteLine("Ingrese su sexo: ");
+            string _sexo = Console.ReadLine();
+
+            Console.WriteLine("Ingrese su peso: ");
+            string _peso = Console.ReadLine();
+
+            Console.WriteLine("Ingrese su altura: ");
+            string _altura = Console.ReadLine();
+
+            Persona Persona1 = new Persona(_nombre, _sueldo, _sexo, _peso, _altura);
+
+            Console.Clear();
+            Console.WriteLine("Datos del usuario: \n\n- Nombre: " + Persona1.Nombre + "\n- Mail: " + Persona1.Mail +  "\n- Sueldo: " + Persona1.Sueldo + " ARS\n- Sexo: " + Persona1.Sexo + "\n- Peso: " + Persona1.Peso + " Kg.\n- Altura: " + Persona1.Altura + "cm");
+            Console.ReadLine();
+        }
+
+        public class Persona
+        {
+            public string Nombre;
+            public string Sueldo;
+            public string Mail;
+            public string Sexo;
+            public string Peso;
+            public string Altura;
+
+            public Persona(string _nombre, string _sueldo, string _sexo, string _peso, string _altura)
+            {
+                Nombre = _nombre;
+                Sueldo = _sueldo;
+                Sexo = _sexo;
+                Peso = _peso;
+                Altura = _altura;
+                Mail = _nombre + "@ejercito.mil.ar";
+            }
+        }
+    }
+}
